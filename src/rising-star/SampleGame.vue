0cac7d755bb777f8b73554913e254c20ccf93d83
HUANPV <phunghuan1508@gmail.com>
2024-05-13 14:32:15 +0700

    update ui

--- src/rising-star/SampleGame.vue
+++ src/rising-star/SampleGame.vue
@@ -1,40 +1,33 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue';
-import Phaser from 'phaser';
-import StartGame from './main';
-import { EventBus } from './EventBus';
+import { onMounted, onUnmounted, ref } from "vue";
+import Phaser from "phaser";
+import StartGame from "./main";
+import { EventBus } from "./EventBus";
 
 // Save the current scene instance
 const scene = ref();
 const game = ref();
 
-const emit = defineEmits(['current-active-scene']);
+const emit = defineEmits(["current-active-scene"]);
 
 onMounted(() => {
+    game.value = StartGame("game-container");
 
-    game.value = StartGame('game-container');
-
-    EventBus.on('current-scene-ready', (scene_instance: Phaser.Scene) => {
-
-        emit('current-active-scene', scene_instance);
+    EventBus.on("current-scene-ready", (scene_instance: Phaser.Scene) => {
+        emit("current-active-scene", scene_instance);
 
         scene.value = scene_instance;
-
     });
-
 });
 
 onUnmounted(() => {
-
     if (game.value) {
         game.value.destroy(true);
         game.value = null;
     }
-
 });
 
 defineExpose({ scene, game });
-
 </script>
 
 <template>

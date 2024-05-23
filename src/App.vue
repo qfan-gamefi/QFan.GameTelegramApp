42ebb0be25f76b2ede0073e9a99d45a0caeac9bd
Nguyen Vuong Hoang <ngvhoang.tn@gmail.com>
2024-05-23 08:49:07 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -43,12 +43,12 @@ export default {
                 last_name = user.last_name || "";
             }
         }
-        window.Telegram.WebApp.initDataUnsafe.user?.id.toString()
+        
         return {
             isTelegramLogin: !!first_name || !!last_name,
             first_name: first_name,
             last_name: last_name,
-            idUser: 5314337740,
+            idUser: window.Telegram.WebApp.initDataUnsafe.user?.id.toString(),
             telegram_bot_link:
                 telegram_bot_link +
                 window.Telegram.WebApp.initDataUnsafe.user?.id || "",

01a69c4e129d721205be8aa96a8ab2cb16193822
HUANPV <phunghuan1508@gmail.com>
2024-10-25 14:52:36 +0700

    fix(inventory): fix css scroll

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -665,8 +665,9 @@ export default {
         },
         async handleYesGiftCode() {
             const res = await userService.giftCode(this.idUser, this.giftCode);
+                        
             if (res.status === 200) {
-                this.renderSuccess("Gift code applied successfully!");
+                this.renderSuccess(`+ ${res?.data?.amount} ${res?.data?.unit}`);
                 this.handleNoGiftCode();
                 this.getInfoUser();
             } else {

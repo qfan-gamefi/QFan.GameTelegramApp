f81f2986d82ed904748fb43848eccf5354b40fe9
Su_fly_007 <phunghuan1508@gmail.com>
2024-12-13 14:23:07 +0700

    remove id home

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -83,7 +83,7 @@ export default {
             isTelegramLogin: !!first_name || !!last_name,
             first_name: first_name,
             last_name: last_name,
-            idUser: dataUserTele?.user?.id?.toString() ?? "2123800227",
+            idUser: dataUserTele?.user?.id?.toString() ?? "",
             telegram_bot_link: telegram_bot_link + dataUserTele?.user?.id || "",
 
             showCoomingSoon: false,

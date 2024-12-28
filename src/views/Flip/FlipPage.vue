a20d623a635cc22e509704b0b9095355856c0832
MilkywayCG <hoquyet.1994@gmail.com>
2024-12-28 12:13:13 +0700

    fix banner flip

--- src/views/Flip/FlipPage.vue
+++ src/views/Flip/FlipPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="wr-flip-page fade-in">
         <img
-            :src="bannerFlip"
+            src="/assets/event/banner-flip.png"
             loading="lazy"
             ref="bannerImage"
             alt="banner-flip"

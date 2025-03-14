acdbaca998bd06c02cd36463d108a7f8ade2526e
Su_fly_007 <phunghuan1508@gmail.com>
2025-03-15 01:09:42 +0700

    add auth x

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,25 @@ import AutoFlip from "./views/Flip/AutoFlip.vue";
 export default {
     components: {
         AutoMining,
-        AutoFlip
+        AutoFlip,
+    },
+    data() {
+        const dataTelegram = window.Telegram?.WebApp?.initDataUnsafe;
+
+        const reqTelegramX = {
+            id: dataTelegram?.user?.id,
+            first_name: dataTelegram?.user?.first_name,
+            username: dataTelegram?.user?.username,
+            photo_url: dataTelegram?.user?.photo_url || "",
+            auth_date: dataTelegram?.auth_date,
+            hash: dataTelegram?.hash,
+        };
+        const jsonString = JSON.stringify(reqTelegramX);
+        const buf = jsonString && Buffer.from(jsonString, "utf-8");
+        const base64X = buf.toString("base64");
+
+        localStorage.setItem("auth-x", base64X);
+        return {};
     },
 };
 </script>
